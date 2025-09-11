923f21153533c5a2c5f12f5687a0b97a4e0a1643
Manikancharana <Kancharanamanikanta26@gmail.com>
2025-09-11 05:50:35 +0530

    expense tracker

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/Header'
+import './App.css'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+function App() {
+  return (
+    <div>
+      <Header/>
+      <div className="container">
+        <Balance/>
+      </div>
+      <div className="expenses">
+        <IncomeExpenses/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/IncomeExpenses.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const IncomeExpenses = () => {
+  return (
+    <div className="inc-exp">
+        <div>
+            
+                <h4>INCOME</h4>
+           <p id="credit"> +$0.00</p>
+        </div>
+        <div>
+            <h4>EXPENSES</h4>
+           <p id="expenses"> -$0.00</p>
+        </div>
+    </div>
+  )
+}
+
+export default IncomeExpenses
