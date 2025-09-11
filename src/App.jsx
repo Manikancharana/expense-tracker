43e2e973a74d795dfac3831f94b5ae25d9a8b792
Manikancharana <Kancharanamanikanta26@gmail.com>
2025-09-11 17:09:57 +0530

    transaction

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header'
 import './App.css'
 import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
 function App() {
   return (
     <div>
@@ -12,6 +14,8 @@ function App() {
       </div>
       <div className="expenses">
         <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
       </div>
     </div>
   )
---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TransactionList = () => {
+  return (
+    <>
+    <h3>History</h3>
+    <ul id="list" class="list">
+      <li class="minus">Cash <span>-$400</span><button class="delete">x</button>
+      </li>
+    </ul>
+    </>
+  )
+}
+
+export default TransactionList 
